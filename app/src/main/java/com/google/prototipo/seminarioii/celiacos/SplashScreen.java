652bdae202820f0a178c0b5dc322a04d3973478f
Commit de sofia <sanchezvsofia@gmail.com>
2017-11-18 21:25:30 -0300

    Splash + loading screen 2

--- app/src/main/java/com/google/prototipo/seminarioii/celiacos/SplashScreen.java
+++ app/src/main/java/com/google/prototipo/seminarioii/celiacos/SplashScreen.java
@@ -3,9 +3,13 @@ package com.google.prototipo.seminarioii.celiacos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
+    ProgressBar barrita;
+    int progreso = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,11 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 try {
+
+                    barrita = (ProgressBar) findViewById(R.id.progressBar);
+                    barrita.setVisibility(View.VISIBLE);
+                    setProgresoBarrita(progreso);
+
                     sleep(3000);
                     Intent intent = new Intent(getApplicationContext(), ChooserActivity.class);
                     startActivity(intent);
@@ -24,5 +33,21 @@ public class SplashScreen extends AppCompatActivity {
             }
         };
         myThread.start();
+    }
+
+    public void setProgresoBarrita(final int progreso) {
+        barrita.setProgress(progreso);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setProgresoBarrita(progreso + 15);
+            }
+        });
+        thread.start();
     }
 }
